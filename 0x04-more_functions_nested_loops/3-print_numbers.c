d9d8c4242da3cbca62774c1577ffa20ef29f3780
Smart Lazarus <smartlazarus@gmail.com>
2023-02-23 08:07:43 -0800

    a function that prints the numbers, from 0 to 9, followed by a new line

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,11 @@ Return: Always 0.
 
 void print_numbers(void)
 {
-	int c;
+	int ch;
 
-	for (c = 48; c < 58; c++)
+	for (ch = 48; ch < 58; ch++)
 	{
-		_putchar(c);
+		_putchar(ch);
 	}
 	_putchar('\n');
 }
