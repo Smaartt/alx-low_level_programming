f315a87cf1c0420fc179a352afa8bf8736d9ef88
Smart Lazarus <smartlazarus@gmail.com>
2023-03-10 16:36:29 -0800

    program that prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - point of entry 
+ * Description: prints out the name of the program
+ * @argc: number of command line arguements
+ * @argv: array of the command line arguements
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
